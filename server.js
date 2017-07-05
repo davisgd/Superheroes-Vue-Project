527d6b84469e7f8ef1c79e2718965c4af4b108a9
davisgd <davis.diede@gmail.com>
2017-07-05 13:08:07 -0600

    The Struggle Bus is real.  Added mongoose and superhero params.

--- server.js
+++ server.js
@@ -1,7 +1,34 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var Superhero = require('./models/Superhero');
 var app = express();
 var port = 3000;
+
+mongoose.connect('mongodb://localhost/superheroes');
+
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.get('/', function(req, res){
+  res.send(superhero);
+});
+
+app.post('/', function(req, res){
+  var superhero = new Superhero();
+  superhero.name = req.body.name;
+  superhero.superpower = req.body.superpower;
+  superhero.save(function(superhero){
+
+    res.send(superhero);
+  }, function(err){
+    res.send(err);
+    }
+  })
+});
 
 var server = app.listen(port, function(){
   console.log("Listening on port", port);
