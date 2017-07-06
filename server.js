195d171cb8c55f5abddb957d9a06b025e25b2521
davisgd <davis.diede@gmail.com>
2017-07-06 08:03:27 -0600

    Fixed add Superhero code.

--- server.js
+++ server.js
@@ -17,16 +17,15 @@ app.get('/', function(req, res){
   res.send(superhero);
 });
 
-app.post('/', function(req, res){
+app.post('/', function(req, res) {
   var superhero = new Superhero();
   superhero.name = req.body.name;
   superhero.superpower = req.body.superpower;
-  superhero.save(function(superhero){
 
+  superhero.save().then(function(superhero) {
     res.send(superhero);
-  }, function(err){
-    res.send(err);
-    }
+  }, function(err) {
+    res.send("Failed to save :( ")
   })
 });
 
