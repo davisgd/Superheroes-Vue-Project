ecac3828e5ad1a2fc60fa843e663242b9d5f771d
davisgd <davis.diede@gmail.com>
2017-07-14 11:33:16 -0600

    Added get, post, and save routes to villains.js file.

--- routes/superheroes.js
+++ routes/superheroes.js
@@ -2,53 +2,59 @@ var express = require('express');
 var Router = express.Router();
 var Superhero = require('../models/Superhero');
 
-Router.route('/').get(function(req, res){
-  Superhero.find(function(err, superheroes){
-    if(err){
-      res.send(err);
-    }else{
-      res.json({
-        data: superheroes
-      });
-    }
-  })
-}).post(function(req, res){
-  console.log('Hitting the post route');
-  var superhero = new Superhero();
-  superhero.name = req.body.name;
-  superhero.superpower = req.body.superpower;
-  superhero.img = req.body.img;
-
-  superhero.save().then(function(superhero){
-    res.json({
-      message: 'Hero successfully created!',
-      data: superhero
-    }, function(err){
-      res.send(err);
+Router.route('/')
+  .get(function(req, res){
+    Superhero.find(function(err, superheroes){
+      if(err){
+        res.send(err);
+      }else{
+        res.json({
+          data: superheroes
+        });
+      }
     })
   })
-})
+  .post(function(req, res){
+    console.log('Hitting the superhero post route');
+    var superhero = new Superhero();
+    superhero.name = req.body.name;
+    superhero.superpower = req.body.superpower;
+    superhero.img = req.body.img;
 
-Router.route('/:_id').get(function(req, res){
-  Superhero.findById(req.params._id, function(err, superhero){
-    if(err){
-      res.send(err, "He's dead, Jim. X(");
-    }else{
-      res.json({
-        message: "Superhero received.",
-        data: superhero
-      })
+    superhero.save(function(erro, superhero){
+      if(err){
+        res.send("B to the ROKEN", err);
+      }else{
+        res.json({
+          message: "Superhero successfully saved!",
+          data: superheroes
+        })
+      }
     }
   })
-}).delete(function(req, res){
+
+Router.route('/:_id')
+  .get(function(req, res){
+    Superhero.findById(req.params._id, function(err, superhero){
+      if(err){
+        res.send(err, "He's dead, Jim. X(");
+      }else{
+        res.json({
+          message: "Superhero received.",
+          data: superhero
+        })
+      }
+    })
+  })
+  .delete(function(req, res){
     Superhero.remove({ _id: req.params._id  },
-  function(err){
-    if(err){
-      res.send(err, "Uh oh, it broke.");
-    }else {
-      res.send("Superhero was deleted!");
-    }
+    function(err){
+      if(err){
+        res.send(err, "Uh oh, it broke.");
+      }else {
+        res.send("Superhero was deleted!");
+      }
+    })
   })
-})
 
 module.exports = Router;
--- routes/villains.js
+++ routes/villains.js
@@ -2,4 +2,36 @@ var express = require('express');
 var Router = express.Router();
 var Villain = require('../models/Villain');
 
+Router.route('/')
+  .get(function(req, res){
+    Villain.find(function(err, villains){
+      if(err){
+        res.send(err);
+      }else{
+        res.json({
+          message: "Villains successfully retrieved",
+          data: villains
+        });
+      }
+    })
+  })
+  .post(function(req, res){
+    console.log("Hitting the villain post route");
+    var villain = new Villain();
+    villain.name = req.body.name;
+    villain.evilPower = req.body.evilPower;
+    villain.img = req.body.img;
+
+    villain.save(function(err, villain){
+      if(err){
+        res.send("Ya broke it.", err);
+      }else{
+        res.json({
+          message: "Villains successfully saved!",
+          data: villain
+        })
+      }
+    })
+  })
+
 module.exports = Router;
